86ce96e00282d68e0f73023e505f68519808077b
esteban <esteban4012223@gmail.com>
2023-07-04 23:38:36 -0500

    creando la clase de clientes

---
+++ src/models/clientes.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class Cliente(BaseModel):
+    id: int
+    name : str
+    last_name : str
+    second_surname = str
+    num_id : int
+    address : str
+    phone_number : int
+    email : str
