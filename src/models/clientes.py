e7df95b2a2d898065023f6a41210f6cdf3502c2c
esteban <esteban4012223@gmail.com>
2023-07-07 11:20:30 -0500

    agregando validacion de datos en la clase Cliente

--- src/models/clientes.py
+++ src/models/clientes.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class Cliente(BaseModel):
-    id: int
-    name : str
-    last_name : str
-    second_surname : str
+    id: int = Field(ge=1)
+    name : str = Field(min_length=4,max_length=15)
+    last_name : str = Field(min_length=4,max_length=15)
+    second_surname : Optional[str] = Field(min_length=4,max_length=15)
     num_id : int
-    address : str
-    phone_number : int
-    email : str
+    address : str = Field(min_length=4,max_length=15)
+    phone_number : int 
+    email : str = Field(min_length=10,max_length=30)
