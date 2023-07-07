e6cde5db1d34f482a7220fda1f2d2a5dc100dd76
esteban <esteban4012223@gmail.com>
2023-07-07 11:32:54 -0500

    agregando validacion de datos en la clase Category

--- src/models/category.py
+++ src/models/category.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Category(BaseModel):
     
-    id : int
-    description : str
+    id : int = Field(ge=1)
+    description : str = Field(min_length=4,max_length=20)
