f05161e3a91935ef7a49db96dc18c82d3af42655
esteban <esteban4012223@gmail.com>
2023-07-07 11:36:49 -0500

    agregando validacion de datos en la clase Articulos

--- src/models/articulos.py
+++ src/models/articulos.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 class Articulos(BaseModel):
 
-    id : int
+    id : int = Field(ge=1)
     price : int
-    description : str
+    description : str = Field(min_length=4,max_length=20)
     id_category : int
 
     
